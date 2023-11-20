36a64d0b49cf72dff26d668ef07a42b14bb1a850
Frederic Leger <frederic@webofmars.com>
2023-11-20 22:34:02 +0100

    feat: add updated postman collection

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-export APIURL=http://localhost:8000
-cd realworld
-./api/run-api-tests.sh
-cd -
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export APIURL=http://realworld-127-0-0-1.sslip.io:8000
+
+USERNAME=${USERNAME:-u$(date +%s)}
+EMAIL=${EMAIL:-$USERNAME@mail.com}
+PASSWORD=${PASSWORD:-password}
+
+set -x
+
+npx newman run $SCRIPTDIR/../Conduit.postman_collection.json \
+  --delay-request 500 \
+  --global-var "APIURL=${APIURL}" \
+  --global-var "USERNAME=$USERNAME" \
+  --global-var "EMAIL=$EMAIL" \
+  --global-var "PASSWORD=$PASSWORD" \
+  "$@"
